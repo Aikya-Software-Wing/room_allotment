a25f0b2bd34e6b56dd04590614eae5d412b7376c
Rahul0598 <devajjir@gmail.com>
2017-11-13 23:44:11 +0530

    Added Controllers

---
+++ ExamRoomAllocation/Controllers/DepartmentController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using ExamRoomAllocation.Models;
+using System.Net;
+using System.Data.Entity;
+
+namespace ExamRoomAllocation.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private ExamRoomAllocationDb db  = new ExamRoomAllocationDb();
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View(db.Departments.ToList());
+        }
+
+        // GET: Department/Details/5
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if(department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // GET: Department/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Department/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="id,Name")]Department department)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if(ModelState.IsValid)
+                {
+                    db.Departments.Add(department);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }                
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to create , contact sys admin");
+            }
+            return View(department);
+        }
+
+        // GET: Department/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Department department = db.Departments.Find(id);
+            if(department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include ="id, Name")] Department department)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if(ModelState.IsValid)
+                {
+                    db.Entry(department).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("","Updation Failed, contact admin");
+            }
+            return View(department);
+        }
+
+        // GET: Department/Delete/5
+        public ActionResult Delete(int? id, bool? SaveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (SaveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete Failed, try again or call admin.";
+            }
+            Department department = db.Departments.Find(id);
+            if (department == null)
+            {
+                return HttpNotFound();
+            }
+            return View(department);
+        }
+
+        // POST: Department/Delete/5
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Department department = db.Departments.Find(id);
+                db.Departments.Remove(department);
+                db.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Delete", new { id = id, SaveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ExamRoomAllocation/Controllers/DesignationController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExamRoomAllocation.Models;
+using System.Data;
+using System.Net;
+using System.Data.Entity;
+
+namespace ExamRoomAllocation.Controllers
+{
+    public class DesignationController : Controller
+    {
+        private ExamRoomAllocationDb db = new ExamRoomAllocationDb();
+        // GET: Designation
+        public ActionResult Index()
+        {
+            return View(db.Designations.ToList());
+        }
+
+        // GET: Designation/Details/5
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);                
+            }
+            Designation designation = db.Designations.Find(id);
+            if(designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // GET: Designation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Designation/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "id, Name")] Designation designation)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if(ModelState.IsValid)
+                {
+                    db.Designations.Add(designation);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to Create, contact sys admin");
+            }
+            return View(designation);
+        }
+
+        // GET: Designation/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Designation designation = db.Designations.Find(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // POST: Designation/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "id, Name")] Designation designation)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if(ModelState.IsValid)
+                {
+                    db.Entry(designation).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Updation Failed, contact admin");
+            }
+            return View(designation);
+        }
+
+        // GET: Designation/Delete/5
+        public ActionResult Delete(int? id, bool? SaveChangesError = false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (SaveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete Failed, try again or call admin.";
+            }
+            Designation designation = db.Designations.Find(id);
+            if (designation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(designation);
+        }
+
+        // POST: Designation/Delete/5
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Designation designation = db.Designations.Find(id);
+                db.Designations.Remove(designation);
+                db.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Delete", new { id = id, SaveChangesError = true });
+            }
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
