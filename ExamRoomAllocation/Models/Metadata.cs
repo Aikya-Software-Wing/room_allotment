51b850e0776cb8d2aa00b009eb677e220c797c06
Rahul0598 <devajjir@gmail.com>
2018-01-20 11:14:35 +0530

    Added ExamMetadata class

---
+++ ExamRoomAllocation/Models/Metadata.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExamRoomAllocation.Models
+{
+    public class ExamMetadata
+    {
+        [Required]
+        [Display(Name = "Exam Code")]
+        public string Code;
+
+        [Required]
+        [Display(Name = "Exam Start Time")]
+        public Nullable<System.DateTime> ExamTime;
+
+        [Required]
+        [Display(Name = "Exam Name")]
+        public string Name;
+
+        [Required]
+        [Display(Name = "Exam Date")]
+        public Nullable<System.DateTime> Date;
+
+        [Required]
+        [Display(Name = "Department To be Held in")]
+        public Nullable<int> Id;
+
+        [Display(Name = "Session Code")]
+        public int SessionId;
+    }
+}
---
+++ ExamRoomAllocation/Models/PartialClasses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamRoomAllocation.Models
+{
+    [MetadataType(typeof(Exam))]
+    public partial class Exam
+    {
+    }
+}
