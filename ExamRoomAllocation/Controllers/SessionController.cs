8128bbd1a5d11236b87975756b68f4b2c78e1f89
Rahul0598 <devajjir@gmail.com>
2018-01-21 20:58:09 +0530

    Added SessionCreator

---
+++ ExamRoomAllocation/Controllers/SessionController.cs
@@ -0,0 +1,119 @@
+﻿using ExamRoomAllocation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamRoomAllocation.Controllers
+{
+    public class SessionController : Controller
+    {
+        private ExamRoomAllocationEntities db = new ExamRoomAllocationEntities();
+        // GET: Session
+        public ActionResult Index()
+        {
+            return View(db.Sessions.ToList());
+        }
+
+        // GET: Session/Details/5
+        public ActionResult Details(int? id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Session session = db.Sessions.Find(id);
+            if(session==null)
+            {
+                return HttpNotFound();
+            }
+            return View(session);
+        }
+
+        // GET: Session/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Session/Create/SessionName
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sessions.Add(session);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(session);
+        }
+
+        // GET: Session/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Session session = db.Sessions.Find(id);
+            if (session == null)
+            {
+                return HttpNotFound();
+            }
+            return View(session);
+        }
+
+        // POST: Session/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include ="SessionName")]Session session)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Entry(session).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(session);
+        }
+
+        // GET: Session/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Session session = db.Sessions.Find(id);
+            if (session == null)
+            {
+                return HttpNotFound();
+            }
+            return View(session);
+        }
+
+        // POST: Session/Delete/5
+        [HttpPost]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Session session = db.Sessions.Find(id);
+            db.Sessions.Remove(session);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ExamRoomAllocation/Helpers/SessionHelper.cs
@@ -0,0 +1,36 @@
+﻿using ExamRoomAllocation.Models;
+using System.Linq;
+using System.Text;
+
+namespace ExamRoomAllocation.Helpers
+{
+    public class SessionHelper
+    {
+        private ExamRoomAllocationEntities db = new ExamRoomAllocationEntities();
+        private Session session = new Session();
+        public static string CreateSession()
+        {
+            StringBuilder s = new StringBuilder();
+            Exam exam = new Exam();
+            int examHour, examDay;
+            string examMonth;
+
+            examHour = exam.ExamTime.Value.Hour;
+            examDay = exam.Date.Value.Day;
+            examMonth = exam.Date.Value.Month.ToString("MMM");
+
+            s.Append(examDay.ToString());
+            s.Append(examMonth.ToString());
+            s.Append(examMonth);
+            if (examHour < 12)
+            {
+                s.Append("1");
+            }
+            else
+            {
+                s.Append("2");
+            }          
+            return s.ToString();
+        }
+    }
+}
