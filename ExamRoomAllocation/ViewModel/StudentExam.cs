a3f9c7007ed6bafd3ce7e7f2812ad591c0814bce
Rahul0598 <devajjir@gmail.com>
2018-01-25 11:25:21 +0530

    Added StudentExam view model

---
+++ ExamRoomAllocation/ViewModel/StudentExam.cs
@@ -0,0 +1,17 @@
+﻿using ExamRoomAllocation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamRoomAllocation.ViewModel
+{
+    public class StudentExam
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Sem { get; set; }
+        public int DepartmentId { get; set; }
+        public virtual ICollection<Exam> Exam { get; set; }
+    }
+}
