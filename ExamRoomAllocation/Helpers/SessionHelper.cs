310880ffddd552e9a274529a9298a386310a063e
mohithm2 <33551252+mohithm2@users.noreply.github.com>
2018-01-25 21:23:39 +0530

    allocating teachertoexam

--- ExamRoomAllocation/Helpers/SessionHelper.cs
+++ ExamRoomAllocation/Helpers/SessionHelper.cs
@@ -13,14 +13,14 @@ namespace ExamRoomAllocation.Helpers
         public static string CreateSession(Exam exam)
         {
             StringBuilder s = new StringBuilder();
-            int examHour, examDay, examMonth;            
+            int examHour, examDay, examMonth;
 
             examHour = exam.ExamTime.Value.Hour;
             examDay = exam.Date.Value.Day;
             examMonth = exam.Date.Value.Month;
 
             s.Append(examDay.ToString());
-            switch(examMonth)
+            switch (examMonth)
             {
                 case 1: s.Append("JAN");
                     break;
@@ -58,6 +58,7 @@ namespace ExamRoomAllocation.Helpers
                     s.Append("DEC");
                     break;
             }
+
             if (examHour < 12)
             {
                 s.Append("1");
@@ -65,8 +66,59 @@ namespace ExamRoomAllocation.Helpers
             else
             {
                 s.Append("2");
-            }          
+            }
+            return s.ToString();
+        }
+
+        public static string TimeHelpher(DateTime? CurrentDate)
+        {
+            StringBuilder s = new StringBuilder();
+            int  examDay, examMonth;
+            examDay = CurrentDate.Value.Day;
+            examMonth = CurrentDate.Value.Month;
+            s.Append(examDay.ToString());
+            switch (examMonth)
+            {
+                case 1:
+                    s.Append("JAN");
+                    break;
+                case 2:
+                    s.Append("FEB");
+                    break;
+                case 3:
+                    s.Append("MAR");
+                    break;
+                case 4:
+                    s.Append("APR");
+                    break;
+                case 5:
+                    s.Append("MAY");
+                    break;
+                case 6:
+                    s.Append("JUN");
+                    break;
+                case 7:
+                    s.Append("JUL");
+                    break;
+                case 8:
+                    s.Append("AUG");
+                    break;
+                case 9:
+                    s.Append("SEP");
+                    break;
+                case 10:
+                    s.Append("OCT");
+                    break;
+                case 11:
+                    s.Append("NOV");
+                    break;
+                case 12:
+                    s.Append("DEC");
+                    break;
+            }
             return s.ToString();
         }
     }
+
+   
 }
---
+++ ExamRoomAllocation/Helpers/examtoteacher.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using ExamRoomAllocation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExamRoomAllocation.Controllers;
+
+namespace ExamRoomAllocation.Helpers
+{
+    public class examtoteacher
+    {
+        private ExamRoomAllocationEntities db = new ExamRoomAllocationEntities();
+
+        private List<Session> listofsessions(DateTime currendate)
+        {
+            try
+            {
+                string temp = SessionHelper.TimeHelpher(currendate);
+                return db.Sessions.Where(s => s.Name.Remove(0, s.Name.Length - 1) != temp).ToList();
+            }
+            catch (NullReferenceException)
+            {
+
+                // to insert alert no session available
+                throw;
+            }
+        }
+        private List<Exam> examinsession(Session session)
+        {
+            try
+            {
+                return db.Exams.Where(c => c.SessionId == session.Id).ToList();
+            }
+            catch (NullReferenceException)
+            {
+
+                // to insert alert no exam in session available
+                throw;
+            }
+        }
+
+        private List<Teacher> teachernotinsamedept(Exam exam)
+        {
+            try
+            {
+                return db.Teachers.Where(t => t.Department != exam.Department).ToList();
+            }
+            catch (NullReferenceException)
+            {
+                // to insert alert no teacher available
+                throw;
+            }
+        }
+
+
+
+        public int Index()
+        {
+            db.Exams.OrderBy(e => e.Date);
+            Exam last = db.Exams.LastOrDefault();
+            Exam first = db.Exams.FirstOrDefault();
+            DateTime startdate = first.Date.GetValueOrDefault();
+            DateTime enddate = last.Date.GetValueOrDefault();
+
+            while (DateTime.Compare(startdate, enddate) < 0)
+            {
+                List<Teacher> teacherassignedinthesamedate = new List<Teacher>(); 
+                List<Session> sessions = listofsessions(startdate);
+                foreach (var session in sessions)
+                {
+                    List<Exam> ExamInSession = examinsession(session);
+                    foreach (var exam in ExamInSession)
+                    {
+                        List<Room> RoomconductingExam = exam.Rooms.ToList();
+                        foreach (var room in RoomconductingExam)
+                        {
+                            List<Teacher> TeacherNotInSamedept = teachernotinsamedept(exam);
+                            TeacherNotInSamedept.OrderByDescending(e => e.TeacherPriority);
+                            foreach (var teacher in TeacherNotInSamedept)
+                            {
+                                
+                                int count = teacher.Exams.Count();
+                                if (count <= 8)
+                                {
+                                    teacher.Exams.Add(exam);
+                                    break;
+                                }
+                            }
+                            RoomconductingExam.RemoveAll(r => r.No == room.No);
+                        }
+                        ExamInSession.RemoveAll(e => e.Code == exam.Code);
+                    }
+                    sessions.RemoveAll(s => s.Id == session.Id);
+                }
+                startdate.AddDays(1);
+            }
+            return 0;
+        }
+    }
+}
