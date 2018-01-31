2de656ee84fe0fb7c06eb2c4d6c8ecfe42a68111
Rahul0598 <devajjir@gmail.com>
2018-01-31 20:10:59 +0530

    Added Results controller and views.

---
+++ ExamRoomAllocation/Controllers/ResultsController.cs
@@ -0,0 +1,75 @@
+﻿using ExamRoomAllocation.Helpers;
+using ExamRoomAllocation.Models;
+using ExamRoomAllocation.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamRoomAllocation.Controllers
+{
+    public class ResultsController : Controller
+    {
+        private ExamRoomAllocationEntities db = new ExamRoomAllocationEntities();
+
+        // GET: Results
+        public ActionResult Index()
+        {
+            examtoteacher assignExam = new examtoteacher();
+            StudentHelpher stud = new StudentHelpher();
+            assignExam.Index();
+            stud.Index();
+            //return View();
+            return RedirectToAction("RoomsIndex");
+        }
+
+        public ActionResult RoomsIndex()
+        {
+            var rooms = db.Rooms.ToList();
+            return View(rooms);
+        }
+
+        public ActionResult RoomSession(int id)
+        {
+            int RoomId = id;
+            TempData["ID"] = RoomId;
+            var sessions = db.Sessions.ToList();
+            return View(sessions);
+        }
+
+        public ActionResult RoomDetails(int sessionId)
+        {
+            RoomViewModel roomViewModel = new RoomViewModel();
+
+            int RoomId = Convert.ToInt32(TempData["ID"]);
+            Room room = db.Rooms.Find(RoomId);
+
+            List<string> studentsList = new List<string>();
+            var students = db.RoomStudents.Where(r => r.Room_Id == RoomId && r.Session_Id == sessionId).ToList();
+            foreach (var student in students)
+            {
+                studentsList.Add(student.Student_Id);
+            }
+            roomViewModel.Students = studentsList;
+
+            List<string> examsList = new List<string>();
+            var exams = db.Database.SqlQuery<string>("SELECT DISTINCT Exam_Code FROM RoomExams JOIN Exam ON RoomExams.Exam_Code = Exam.Code").ToList();
+            foreach (var exam in exams)
+            {
+                examsList.Add(exam);
+            }
+            roomViewModel.ExamCode = examsList;
+
+            Session session = db.Sessions.Find(sessionId);
+            roomViewModel.SessionName = session.Name;
+
+            Teacher teacher = new Teacher();
+            TeacherRoom teacherId = db.TeacherRooms.Where(r => r.Room_Id == RoomId && r.Session_Id == sessionId).FirstOrDefault();
+            teacher = db.Teachers.Find(teacherId.Teacher_Id);
+            roomViewModel.TeacherName = teacher.Name;
+
+            return View(roomViewModel);
+        }
+    }
+}
---
+++ ExamRoomAllocation/ViewModel/RoomViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ExamRoomAllocation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamRoomAllocation.ViewModel
+{
+    public class RoomViewModel
+    {
+        public string TeacherName { get; set; }
+        public string TeacherId { get; set; }      
+        public string SessionName { get; set; }
+        public List<string> ExamCode { get; set; }
+        public List<string> Students { get; set; }
+    }
+}
